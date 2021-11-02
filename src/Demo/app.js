f7d0b0f9712aec4b5144bfbb971d9cc7de651c4d
coulsonzero <coulson2021@163.com>
2021-11-02 09:23:09 +0800

    2021-11-02

---
+++ src/Demo/app.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    // const btn = document.querySelector(".btn");
+    // let isbtnCheck = getComputedStyle(btn, 'before').getPropertyValue('left');
+    // // console.log(isbtnCheck);
+    // let style =document.createElement("style");
+    // btn.onclick = function() {
+    //     // let text = style.createTextNode('input[type="radio"]:checked::before{left: 42px;}');
+    //     // style.appendChild(text);
+    //     // document.body.appendChild(style);
+
+    //     // if(isbtnCheck == 0) {
+    //     //     console.log("ok");
+    //     //     getComputedStyle(btn, 'before').style.left = 42;
+    //     // }else {
+    //     //     console.log("sss");
+    //     //     getComputedStyle(btn, 'before').style.left = 0;
+    //     // }
+    // }
+};
