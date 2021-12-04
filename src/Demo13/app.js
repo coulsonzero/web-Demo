b74a849fae4b52a776607e42af286c6172ed961a
coulsonzero <coulson2021@163.com>
2021-12-05 00:19:09 +0800

    update app.js

--- src/Demo13/app.js
+++ src/Demo13/app.js
@@ -1,12 +1,13 @@
 window.onload = function() {
-    $(".topbar-link").click(function () {
-        $(".topbar-link").removeClass("is-active");
-        $(this).addClass("is-active");
-        if (this.innerHTML === "About") {
-            window.open('http://www.baidu.com','_blank');
-        } else if (this.innerHTML === "Service") {
-            window.open('../Demo12/index.html','_blank');
-        }
+    $(".nav-link").mouseover(function () { 
+        $(".nav-link").removeClass("hover active");
+        $(this).addClass("hover active");
     });
-    
+    /*
+    if (this.innerHTML === "About") {
+        window.open('http://www.baidu.com','_blank');
+    } else if (this.innerHTML === "Service") {
+        window.open('../Demo12/index.html','_blank');
+    }
+    */
 }
