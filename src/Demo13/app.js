05583cf4617e03ee171dfac8ba17791a445f2a42
coulsonzero <coulson2021@163.com>
2021-12-04 15:31:35 +0800

    2021-12-04

--- src/Demo13/app.js
+++ src/Demo13/app.js
@@ -1,7 +1,12 @@
 window.onload = function() {
     $(".topbar-link").click(function () {
-        console.log(this)
         $(".topbar-link").removeClass("is-active");
         $(this).addClass("is-active");
+        if (this.innerHTML === "About") {
+            window.open('http://www.baidu.com','_blank');
+        } else if (this.innerHTML === "Service") {
+            window.open('../Demo12/index.html','_blank');
+        }
     });
+    
 }
